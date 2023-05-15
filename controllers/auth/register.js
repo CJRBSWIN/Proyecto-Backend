be0ceb3ba0a1f9469d4a4edde04f48521d714a4d
CJRBSWIN <cjrbswin@gmail.com>
2023-05-15 23:54:00 +0200

    Actualizando ando

--- controllers/auth/register.js
+++ controllers/auth/register.js
@@ -1,4 +1,23 @@
+const { register } = require(".");
 const db = require("../../configs/db");
+
+app.get("/", (req, res) => {
+    res.send("Hello World!");
+});
+
+app.post("/register", async (req, res) => {
+    const {name, password} = req.body;
+
+    try {
+        await db.query(sql.unsafe`
+        INSERT INTO users (name, password) VALUES (${name}, ${password})
+        `);
+    } catch (error){
+        console.log(error);
+    }
+});
+
+register()
 
 module.exports = (db) => async (req, res, next) => {
     res.status(200).json({
